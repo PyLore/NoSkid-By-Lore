16448bcef967555fecf5fe0cbb3f88b527e02eea
Lore <88983497+PyLore@users.noreply.github.com>
2022-06-07 17:17:05 -0400

    Update spoof.py

--- data/spoof.py
+++ data/spoof.py
@@ -5,18 +5,18 @@ from .theme   import C
 USERAGENTS = open('data/useragents.txt').read().splitlines()
 REFERRERS  = open('data/referrers.txt').read().splitlines()
 
-proxy_urls = [
+PROXY_URLS = (
     'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=3000&country=all&ssl=all&anonymity=all&simplified=true',
     'https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=yes&timeout=2000'
-]
+)
 
-PROXIES = set()
+PROXIES    = set()
 while True:
     try:
-        for url in proxy_urls:
-            PROXIES.update(get(url).text)
+        for url in PROXY_URLS:
+            PROXIES.update(get(url).text.splitlines())
     except:
         print(f'{C.red}• {C.white}Error occured while attempting to download proxies.')
-        pass
+        
     print(f'{C.lime}• {C.white}Downloaded proxies.')
-    break    
+    break
